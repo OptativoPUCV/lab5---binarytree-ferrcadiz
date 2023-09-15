b45f4f4f79129c9e26761082a92287fcf24a6d61
Replit user <>
2023-09-15 05:39:36 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -111,7 +111,7 @@ void removeNode(TreeMap *tree, TreeNode *node) {
   }
   else{
     if(node -> left == NULL){
-      //noso con un hijo (izquierdo)
+      //nodo con un hijo (izquierdo)
       if(node -> parent == NULL)
     }
   }
